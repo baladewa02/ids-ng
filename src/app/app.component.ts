9b4cfe60cf836ac9606faeef285d3c699823e9db
baladewa02 <baladewa@gmail.com>
2026-02-24 12:48:03 +0000

    initial project

---
+++ src/app/app.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'IDS Enterprise Angular Starter';
+  idsVersion = '20.2.1';
+}
